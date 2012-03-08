1ed26103648f589b5669fa15cada958d8cf1d847
Krzysiek Huszcza <kwh109@ic.ac.uk>
2012-03-08 22:59:02 +0000

    runClient implmeneted and testes

--- Rest/src/main/java/uk/ac/imperial/doc/rest/MsgReader.java
+++ Rest/src/main/java/uk/ac/imperial/doc/rest/MsgReader.java
@@ -38,6 +38,7 @@
  * @author Phil Levis <pal@cs.berkeley.edu>
  */
 
+package uk.ac.imperial.doc.rest;
 
 import java.util.*;
 
@@ -45,79 +46,77 @@ import net.tinyos.message.*;
 import net.tinyos.packet.*;
 import net.tinyos.util.*;
 
-public class MsgReader implements net.tinyos.message.MessageListener {
+public class MsgReader implements MessageListener {
 
-  private MoteIF moteIF;
-  
-  public MsgReader(String source) throws Exception {
-    if (source != null) {
-      moteIF = new MoteIF(BuildSource.makePhoenix(source, PrintStreamMessenger.err));
-    }
-    else {
-      moteIF = new MoteIF(BuildSource.makePhoenix(PrintStreamMessenger.err));
-    }
-  }
+    private MoteIF moteIF;
 
-  public void start() {
-  }
-  
-  public void messageReceived(int to, Message message) {
-
-      // TODO
-      // convert message to JSON
-      // Send it to rest client
-      // RESTClient.postDataSamples(message) or smth
-  }
-
-  
-  private static void usage() {
-    System.err.println("usage: MsgReader [-comm <source>] message-class [message-class ...]");
-  }
-
-  private void addMsgType(Message msg) {
-    moteIF.registerListener(msg, this);
-  }
-  
-  public static void main(String[] args) throws Exception {
-    String source = null;
-    Vector v = new Vector();
-    if (args.length > 0) {
-      for (int i = 0; i < args.length; i++) {
-	if (args[i].equals("-comm")) {
-	  source = args[++i];
-	}
-	else {
-	  String className = args[i];
-	  try {
-	    Class c = Class.forName(className);
-	    Object packet = c.newInstance();
-	    Message msg = (Message)packet;
-	    if (msg.amType() < 0) {
-		System.err.println(className + " does not have an AM type - ignored");
-	    }
-	    else {
-		v.addElement(msg);
-	    }
-	  }
-	  catch (Exception e) {
-	    System.err.println(e);
-	  }
-	}
-      }
-    }
-    else if (args.length != 0) {
-      usage();
-      System.exit(1);
+    public MsgReader(String source) throws Exception {
+        if (source != null) {
+            moteIF = new MoteIF(BuildSource.makePhoenix(source, PrintStreamMessenger.err));
+        } else {
+            moteIF = new MoteIF(BuildSource.makePhoenix(PrintStreamMessenger.err));
+        }
     }
 
-    MsgReader mr = new MsgReader(source);
-    Enumeration msgs = v.elements();
-    while (msgs.hasMoreElements()) {
-      Message m = (Message)msgs.nextElement();
-      mr.addMsgType(m);
+    public void start() {
     }
-    mr.start();
-  }
+
+    public void messageReceived(int to, Message message) {
+
+        // TODO
+        // convert message to JSON
+        // Send it to rest client
+        // RESTClient.postDataSamples(message) or smth
+    }
+
+
+    private static void usage() {
+        System.err.println("usage: MsgReader [-comm <source>] message-class [message-class ...]");
+    }
+
+    private void addMsgType(Message msg) {
+        moteIF.registerListener(msg, this);
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Initializing message reader");
+
+        String source = null;
+        Vector v = new Vector();
+        if (args.length > 0) {
+            for (int i = 0; i < args.length; i++) {
+                if (args[i].equals("-comm")) {
+                    source = args[++i];
+                } else {
+                    String className = args[i];
+                    try {
+                        Class c = Class.forName(className);
+                        Object packet = c.newInstance();
+                        Message msg = (Message) packet;
+                        if (msg.amType() < 0) {
+                            System.err.println(className + " does not have an AM type - ignored");
+                        } else {
+                            v.addElement(msg);
+                        }
+                    } catch (Exception e) {
+                        System.err.println(e);
+                    }
+                }
+            }
+        } else if (args.length != 0) {
+            usage();
+            System.exit(1);
+        }
+
+        MsgReader mr = new MsgReader(source);
+        Enumeration msgs = v.elements();
+        while (msgs.hasMoreElements()) {
+            Message m = (Message) msgs.nextElement();
+            mr.addMsgType(m);
+        }
+        mr.start();
+    }
 
 
 }
